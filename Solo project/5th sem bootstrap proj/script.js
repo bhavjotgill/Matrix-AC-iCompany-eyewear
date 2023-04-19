e473a90c7f7cf604f009dd1a49c74af98c6f5612
bhavjotgill <75017284+bhavjotgill@users.noreply.github.com>
2023-04-19 23:54:03 +0530

    Add files via upload

---
+++ Solo project/5th sem bootstrap proj/script.js
@@ -0,0 +1,30 @@
+function func11() {
+    document.getElementById('collImg1').src = "pics/pic1hover.jpg";
+}
+function func12() {
+    document.getElementById('collImg2').src = "pics/pic2hover.jpg";
+}
+function func13() {
+    document.getElementById('collImg3').src = "pics/pic3hover.jpg";
+}
+function func21() {
+    document.getElementById('collImg1').src = "pics/pic1.webp";
+}
+function func22() {
+    document.getElementById('collImg2').src = "pics/pic2.jpg";
+}
+function func23() {
+    document.getElementById('collImg3').src = "pics/pic3.jpg";
+}
+function funcShop() {
+    window.open('shop.html', '_parent');
+}
+function mainPageFb() {
+    window.open("https://www.facebook.com/profile.php?id=100081227414026","_parent");
+}
+function mainPageYt() {
+    window.open("https://www.youtube.com/channel/UCwkkZGlvvJPpoba0Aui3eJQ","_parent");
+}
+function mainPageLI() {
+    window.open("https://www.linkedin.com/in/bhavjot-gill-8469a721a/","_parent");
+}
